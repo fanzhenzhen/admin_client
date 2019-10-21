da5ee37d14163afb4e9749abd33f6739a8736f6c
fanzhen <fanz@cpwxw.net>
2019-10-21 11:12:54 +0800

    commit_rules

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -10,6 +10,7 @@ class Login extends Component {
 
   }
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
       <div className="login">
         <header className="login-header">
@@ -17,20 +18,28 @@ class Login extends Component {
           <h1>后台管理系统</h1>
         </header>
         <div className="login-content">
-          <h1>用户登陆</h1>
+          <h1>用户登录</h1>
           <Form onSubmit={this.handleSubmit} className="login-form">
             <Item>
-              <Input
+                {getFieldDecorator('username', {
+                rules: [{ required: true, message: 'Please input your username!' }],
+              })(
+                <Input
                   prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                   placeholder="用户名"
-                />
+                />,
+              )}
             </Item>
             <Item>
-              <Input
+                {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+              })(
+                <Input
                   prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
                   type="password"
                   placeholder="密码"
-                />
+                />,
+              )}
             </Item>
             <Item>
             <Button type="primary" htmlType="submit" className="login-form-button">登陆</Button>

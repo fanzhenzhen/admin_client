b3a4ec1ffa891ca5e6e9f0a405dd602f09049780
fanzhen <fanz@cpwxw.net>
2019-10-25 14:41:09 +0800

    commit-weather

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,8 @@
 函数的返回值是promise
 */
 import ajax from './ajax'
+import jsonp from 'jsonp'
+import {message} from 'antd'
 
 export const reqLogin = ({username,password})=>ajax({
     url:'/login',
@@ -12,3 +14,20 @@ export const reqLogin = ({username,password})=>ajax({
 
 //获取用户列表
 export const getUsers = ()=>ajax.get('/manage/user/list')
+
+//获取天气预报信息
+//使用jsonp请求
+export const reqWeather=(city)=>{
+  return new Promise((resolve,reject)=>{
+    const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+    jsonp(url,{},(err,data)=>{
+      if(!err){
+      const {dayPictureUrl,weather} = data.results[0].weather_data[0]
+      resolve({dayPictureUrl,weather})
+      }else{
+       message.error('请求出错了！')
+       return new Promise(()=>{})
+      }
+    })
+  })
+}
--- src/containers/admin/header/index.jsx
+++ src/containers/admin/header/index.jsx
@@ -8,16 +8,19 @@ import screenfull from 'screenfull';
 import './index.less'
 import LinkButton from '../../../components/link-button'
 import { removeUserToken } from "../../../redux/action-creators/user";
+import {reqWeather} from '../../../api/index'
 
 @connect(
   state=>({username:state.user.user.username}),
-  {removeUserToken}
+  {removeUserToken,reqWeather}
 )
 @withRouter
  class Home extends Component {
   state = {
      currentTime: dayjs().format('YYYY-MM-DD HH:mm:ss'),
-     isFullScreen:false
+     isFullScreen:false,
+     dayPictureUrl:'',//天气图片默认地址
+     weather:''//天气默认文本
   }
   logout=()=>{
     Modal. confirm({
@@ -35,6 +38,13 @@ import { removeUserToken } from "../../../redux/action-creators/user";
       screenfull.toggle();
     }
   }
+  showWeather=async()=>{
+   const {dayPictureUrl,weather}= await reqWeather('北京')
+   this.setState({
+    dayPictureUrl,
+    weather
+   })
+  }
   componentDidMount(){
     // 启动循环定时器, 每隔1s, 更新显示当前时间
     this.intervalId = setInterval(() => {
@@ -47,13 +57,16 @@ import { removeUserToken } from "../../../redux/action-creators/user";
         isFullScreen:!this.state.isFullScreen
       })
     })
+    //请求天气接口
+    this.showWeather();
+   
   }
   componentWillUnmount(){
     clearInterval(this.intervalId)
   }
   render() {
     const path = this.props.location.pathname
-    const {currentTime,isFullScreen} = this.state
+    const {currentTime,isFullScreen,dayPictureUrl,weather} = this.state
     return (
       <div className="header">
       <div className="header-top">
@@ -67,8 +80,8 @@ import { removeUserToken } from "../../../redux/action-creators/user";
         <div className="header-bottom-left">{path}</div>
         <div className="header-bottom-right">
           <span>{currentTime}</span>
-          <img src="http://api.map.baidu.com/images/weather/day/xiaoyu.png" alt="weather"/>
-          <span>小雨转多云</span>
+          <img src={dayPictureUrl} alt="weather"/>
+          <span>{weather}</span>
         </div>
       </div>
     </div>

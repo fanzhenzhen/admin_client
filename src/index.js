0930a3263e5a2c8bed5e59f616e09600b97adaab
fanzhen <fanz@cpwxw.net>
2019-10-30 20:06:46 +0800

    com-spin

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
+import {Spin} from 'antd'
 
 import App from './App'
 import store from './redux/store'
 
 
-ReactDOM.render((<Provider store={store}><App /></Provider>), document.getElementById('root'));
+ReactDOM.render((
+  <React.Suspense fallback={<Spin size="large"/>}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.Suspense>
+    ), 
+  document.getElementById('root')
+  );
